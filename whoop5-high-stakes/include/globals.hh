70c16d1878e1d323b982daac333b6202facdf803
Jeremy Leicht <94857347+Jeremy924@users.noreply.github.com>
2024-09-11 16:49:51 -0500

    added more comments

--- whoop5-high-stakes/include/globals.hh
+++ whoop5-high-stakes/include/globals.hh
@@ -1,60 +1,91 @@
-#ifndef GLOBALS_H
+#ifndef GLOBALS_H  // these are header guards: https://www.geeksforgeeks.org/header-guard-in-c/#
 #define GLOBALS_H
 
 #include "rev/rev.hh"
 
+// all lines that start with a # are directives: https://www.geeksforgeeks.org/cpp-preprocessors-and-directives/
+
+// #define statements are used to define constants. 
+// the first parameter is the name, and everything after it will be directly substituted in the code
+// ex. #define PI 3.14159
+//    double circumference = 2 * PI * radius;
+// equivalent to
+//    double circumference = 2 * 3.14159 * radius;
+// definitions make code easier to read, however in C++, it is generally recommended to use const variables instead because they have type safety
 
 // Ports
-#define LEFT_MOTOR_GROUP {1, 2, 3}
-#define RIGHT_MOTOR_GROUP {4, 5, 6}
-#define INTAKE {7, 8}
+#define LEFT_MOTOR_GROUP {-11, -12, -13, -18} // this is a vector, which is like a list in python or java
+#define RIGHT_MOTOR_GROUP {2, 3, 5, 6}
+#define INTAKE {17, -7}
 
-#define IMU_PORT 20
-#define FWD_PORT 1
-#define LAT_PORT 2
+#define IMU_PORT 4    // these numbers correspond to the ports on the brain
+#define FWD_PORT -1   // **when they are negative, it means that they are flipped. Ex. -1 for a motor will make motor go backwards when you input a positive value**
+#define LAT_PORT -14
 
-#define BEAM_BREAK_PORT 'A'
+#define BEAM_BREAK_PORT 'A'   // 3-wire ports use characters instead of integers
 
 #define BACK_WING_L_PORT 'B'
 #define BACK_WING_R_PORT 'C'
 #define FRONT_WINGS_PORT 'D'
 #define ODOM_HYDRAULIC_PORT 'E'
 
+// _mm, _in, _deg, ... are units. By using units, the compiler can check that your math is correct (ex. 1_deg + 1_mm is invalid)
 // Parameters
 #define WHEEL_DIAMETER 63.89_mm        // Diameter of forward and sideways wheel
 #define FORWARD_WHEEL_OFFSET -1.125_in // How far to the right of the center of the robot the forward wheel is
 #define LATERAL_WHEEL_OFFSET -1_in     // How far to the rear of the robot the lateral wheel is from the center
 
+
+// tuning constants. These values are determined through a lot of testing
 #define TURN_IKP1 0.18
 #define TURN_IKP2 0.07
 
-
+// AsyncRunners are used to run background threads that update controllers asyncrhonously.
+// With background threads, the frontend user doesn't have to constantly update motor speeds, sensors, etc
+// because it will be done automatically in the background
+// https://pros.cs.purdue.edu/v5/tutorials/topical/multitasking.html
 extern std::shared_ptr<rev::AsyncRunner> odom_runner;
 extern std::shared_ptr<rev::AsyncRunner> reckless_runner;
 extern std::shared_ptr<rev::AsyncRunner> turn_runner;
 
+// TwoRotationInertialOdometry is the odometry system. It automatically tracks the robot's
+// position and angle globally. 
+// https://wiki.purduesigbots.com/software/odometry
 extern std::shared_ptr<rev::TwoRotationInertialOdometry> odom;
+
+// The chassis object represents the motors that are used to drive the robot around (not including intake, etc)
 extern std::shared_ptr<rev::SkidSteerChassis> chassis;
 
+// Reckless is the controller that drives the robot to points on the field.
+// all points given to reckless are global positions. 
+// Reckless does not care about your final angle. To go to a specific angle, you have to use the turn controller.
+// the reckless_runner AsyncRunner controls the motors so the frontend code can do other stuff while reckless is driving
+// because it is async though, you have to make sure not to send other motion controls to the chassis because it could cause conflicts
 extern std::shared_ptr<rev::Reckless> reckless;
+
+// This is the turn controller. It can only do point turns. It turns to a global angle. 
 extern std::shared_ptr<rev::CampbellTurn> turn;
 
-
 // motor ports
-extern pros::MotorGroup left_motor_group;
+extern pros::MotorGroup left_motor_group; // the robots usually have a row of motors on the left and a row of motors on the right
 extern pros::MotorGroup right_motor_group;
 extern pros::MotorGroup intake;
 
-// sensor inputs
-extern pros::IMU imu;
-extern pros::Rotation fwd;
-extern pros::Rotation lat;
+// sensor inputs 
+// https://pros.cs.purdue.edu/v5/api/cpp/imu.html?highlight=imu
+extern pros::IMU imu;     // Intertial sensor. Measures acceleration and rotation using accelerometers and gyroscopes. For our odometry system, this is only used for angle
+
+// https://pros.cs.purdue.edu/v5/api/cpp/rotation.html?highlight=rotation
+extern pros::Rotation fwd; // rotation sensor parrellel to the wheels
+extern pros::Rotation lat; // rotation sensor perpendicular to the wheels
 
 
 // Beam Break
-extern pros::ADIDigitalIn beam_break;
+//https://pros.cs.purdue.edu/v5/api/cpp/adi.html
+extern pros::ADIDigitalIn beam_break; // Consists of a light transmitter and a receiver. If the path between the two is blocked, then the sensor will output a 0, otherwise a 1. 
 
 // hydraulics
+//https://pros.cs.purdue.edu/v5/api/cpp/adi.html
 extern pros::ADIDigitalOut backWingL;
 extern pros::ADIDigitalOut backWingR;
 extern pros::ADIDigitalOut frontWings;
--- whoop5-high-stakes/src/main.cpp
+++ whoop5-high-stakes/src/main.cpp
@@ -1,7 +1,11 @@
+// #include directly copies the content of the specified file into the place where the #include directive is used.
+// when including standard C++ libraries, you use angle brackets. When including your own files, you use quotes.
+//https://en.cppreference.com/w/cpp/preprocessor/include
 #include "main.h"
 #include "rev/rev.hh"
 #include "globals.hh"
 
+// this makes it so you can just put the name of the item rather then putting rev:: before everything
 using namespace rev;
 
 /**
@@ -11,9 +15,11 @@ using namespace rev;
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-
+	// creates a skid steer chassis object, which is a chassis with a group of motors on the left and the right
 	chassis = std::make_shared<rev::SkidSteerChassis>(left_motor_group, right_motor_group);
+
+	// creates the odometry system. This is how the robot tracks its global position on the field. 
+	// the parameters for it are defined in the globals.hh file
 	odom = std::make_shared<rev::TwoRotationInertialOdometry>(
     fwd,      // The forward sensor
     lat,      // The rightward sensor 
@@ -24,19 +30,25 @@ void initialize() {
     LATERAL_WHEEL_OFFSET    // How far to the rear of the robot the lateral wheel is from the center
   );
 
+  // creates a turn controller object. This turn controller can only do point turns
   turn = std::make_shared<CampbellTurn>(chassis, odom, TURN_IKP1, TURN_IKP2);
+
+  // creates a reckless controller object. This is used to drive the robot to points on the field
   reckless = std::make_shared<Reckless>(chassis, odom);
 
-  odom_runner = std::make_shared<rev::AsyncRunner>(odom);
+	// these are the background threads that update reckless, odometry, and the turn controller. 
+    odom_runner = std::make_shared<rev::AsyncRunner>(odom);
 	reckless_runner = std::make_shared<rev::AsyncRunner>(reckless);
 	turn_runner = std::make_shared<rev::AsyncRunner> (turn);
 
+	// initialize the lcd screen
 	pros::lcd::initialize();
 
+	// delay 2 seconds so that the IMU has time to initialize
   pros::delay(2000);
-  odom->reset_position();
 
-  
+  // sets the robot's position to be (0_in, 0_in, 0_deg). This is probably not necessary 
+  odom->reset_position();
 }
 
 /**
@@ -69,22 +81,28 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
+	/*
+	 * The code in this function should drive the robot in a 20in by 20in square.
+	*/
+
 
 	/*
 	 * The following function call will drive the robot to the point 20_in, 0_in. The 0_deg is meaningless,
 	 * but it is still required otherwise there will be a compiler error. 
 	 * The code can be copy and pasted, you just have to change out the coordinates to go to a different spot. 
 	*/
-	reckless->go(RecklessPath().with_segment(
+	reckless->go(
+		RecklessPath().with_segment(
 		RecklessPathSegment(
-			std::make_shared<ConstantMotion>(0.5),
-			std::make_shared<PilonsCorrection>(4, 0.3_in),
-			std::make_shared<SimpleStop>(0.03_s, 0.15_s, 0.3), 
-			{ 20_in, 0_in, 0_deg }, 0_in)
+			std::make_shared<ConstantMotion>(0.5),             // tells the robot to move at 50% power
+			std::make_shared<PilonsCorrection>(4, 0.3_in), // if the robot is 0.3in or more off the path, then it will start correcting that path
+			std::make_shared<SimpleStop>(0.03_s, 0.15_s, 0.3),   // robot will soft stop if it is 0.15 seconds from the finish, and hard stop when it is 0.03 seconds from the finish. Soft stop means that the speed is set to 30% power. Hard stop means that the brakes are applied
+			{ 20_in, 0_in, 0_deg },               // the target global position. Position 0, 0 is where the robot starts. the 0_deg is meaningless but it has to be included for syntax reasons
+			0_in)                                          // tells the robot to stop 0_in from the target
 	));
 	reckless->await(); // don't run the next code until the robot has reached the target
 
-	// turn the robot 90 degrees
+	// turn the robot 90 degrees with a max power of 70%
 	turn->turn_to_target_absolute(0.7, 90_deg);
 	turn->await();
 
@@ -150,19 +168,19 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::lcd::set_text(0, "Hello PROS User!");
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::lcd::set_text(0, "Hello PROS User!"); // shows text on the LCD. Useful for testing purposes. 
+	pros::Controller master(pros::E_CONTROLLER_MASTER); // used to get inputs from the users's controller
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		// read from the controller the the left and right joystick values. These values are from -127 to 127, which the same range used by the motors. 
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
+		// set the motors powers to be the same as the joystick values
 		left_motor_group = left;
 		right_motor_group = right;
-
+		
+		// delay 20 milliseconds. **This is important because it gives the background threads a change to run**
 		pros::delay(20);
 	}
 }
